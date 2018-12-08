c319c92736f03426a14529f1071c6111f81a3902
Zart Seri <zartseri@yahoo.com>
2018-12-08 11:53:22 -0500

    Basic Node App assignment: completed

---
+++ main.js
@@ -0,0 +1,19 @@
+const greetings = require("./greeting.js");
+const sendoffs = require("./sendoff.js");
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+// "Bye"
+sendoffs.sendoffEnglish();
+
+// "Adios"
+sendoffs.sendoffSpanish();
---
+++ sendoff.js
@@ -0,0 +1,7 @@
+exports.sendoffEnglish = function(){
+  console.log("Bye")
+}
+
+exports.sendoffSpanish = function(){
+  console.log("Adios")
+}
